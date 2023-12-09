b24bf64e90d908ca5e5ee542cf1a398a03077a58
dave-rushabh <rushabh.dave24@gmail.com>
2023-12-10 00:56:09 +0530

    deployment issue testing

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
