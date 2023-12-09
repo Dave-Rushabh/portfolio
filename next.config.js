cf6d2edb8f43e6d1703db40b18d761bbd2f20030
dave-rushabh <rushabh.dave24@gmail.com>
2023-12-10 00:27:49 +0530

    Deployment issue testing with API routes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+}
 
 module.exports = nextConfig
